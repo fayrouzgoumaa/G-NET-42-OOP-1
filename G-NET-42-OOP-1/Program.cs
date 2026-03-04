2aacc602f677eaf267b08a4b8644c206183560ff
fayro <fayro@FAYROUZGOUMAA>
2026-03-04 23:14:54 +0200

    Q1

--- G-NET-42-OOP-1/Program.cs
+++ G-NET-42-OOP-1/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace G_NET_42_OOP_1
 {
+    class PersonClass
+    {
+        public string Name;
+    }
+    struct PersonStruct
+    {
+        public string Name;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            // CLASS behavior
+            PersonClass p1 = new PersonClass();
+            p1.Name = "Fayrouz";
+
+            PersonClass p2 = p1;  // Copies reference
+            p2.Name = "Goumaa";
+
+            Console.WriteLine(p1.Name); // Output: Goumaa
+
+
+            // STRUCT behavior
+            PersonStruct s1;
+            s1.Name = "Fayrouz";
+
+            PersonStruct s2 = s1; // Copies value
+            s2.Name = "Goumaa";
+
+            Console.WriteLine(s1.Name); // Output: Fayrouz
+            #endregion
+
         }
     }
 }
